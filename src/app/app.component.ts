fc8661ec6290476b075a8bf9483e7d6d7ca73fdb
Alvaro Camillo Neto <alvaro.camilloneto@gmail.com>
2020-10-19 11:58:26 -0300

    teste

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,20 @@
 import { Component } from '@angular/core';
 
+import { PoMenuItem } from '@po-ui/ng-components';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'teste-strict';
+
+  readonly menus: Array<PoMenuItem> = [
+    { label: 'Home', action: this.onClick.bind(this) }
+  ];
+
+  onClick() {
+    alert('Clicked in menu item')
+  }
+
 }
